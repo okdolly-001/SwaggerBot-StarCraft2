b4351a3e8412915ff1781a6309140a9769a287dd
ariathefirst <ariang@ucdavis.edu>
2018-05-12 16:37:36 -0700

    modified replay file

--- parsed_replays/GlobalFeatureVector/Protoss_vs_Protoss/Protoss/open.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from tempfile import TemporaryFile
-outfile = TemporaryFile()
-data = np.load('2@ffff487609735303a99e2d156b9d28163d0b371fe4e69cc31940ce66d2f6bb9e.SC2Replay.npz')
-print(data)
-
-
-

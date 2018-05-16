d3eb8db892a934adb61cf282617ef8ac2ef0a484
ariathefirst <ariang@ucdavis.edu>
2018-05-16 10:10:03 -0700

    removed incomplete replay files

--- parsed_replays/open.py
+++ parsed_replays/open.py
@@ -2,13 +2,17 @@ import numpy as np
 import pandas as pd
 import csv
 from scipy import sparse
+import pickle
 
 F = np.asarray(sparse.load_npz('./GlobalFeatureVector/Protoss_vs_Protoss/Protoss/1@0041f867a19b27400acbfb497df38a7ebfb4eec679b3fd30f370fc24c087b096.SC2Replay.npz').todense())
-np.savetxt('file', F, fmt='%s') # .csv is a dataframe
+np.savetxt('f.csv', F, delimiter=",", fmt='%s') # .csv is a dataframe
 
-df = pd.read_csv('file', delimiter=' ')
-df.to_csv('file.csv')
+# df = pd.read_csv('file', delimiter=',')
+# df.to_csv('file.csv')
 
-print(F.shape)
-#for state in F:
-# 	print(state[317:].mean())
+with open('f.csv', 'rb') as f:
+	file = np.load(f)
+
+# # print(F.shape)
+for state in file:
+	print(state[317:].mean())

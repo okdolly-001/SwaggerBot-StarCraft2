c881737b85a0419d00b3161c0d954b442e3615c9
ariathefirst <ariang@ucdavis.edu>
2018-06-08 08:02:21 -0700

    plots percentage against episode now

--- comprehensiveAgent/SWARMBOT/graph.py
+++ comprehensiveAgent/SWARMBOT/graph.py
@@ -3,6 +3,8 @@ import csv
 
 with open("reward.txt", 'rU') as f:
     res = csv.reader(f, delimiter=",")
+
+    win_so_far, loss_so_far, tie_so_far = [], [], []
     cur_win_count, cur_loss_count, cur_tie_count = 0, 0, 0
 
     percetage_win_so_far, percetage_loss_so_far, percetage_tie_so_far = [], [], []
@@ -10,46 +12,43 @@ with open("reward.txt", 'rU') as f:
 
     game_count = 0
 
+    count = list(range(97))
+
     for row in res:
         row = ''.join(row)  # converts list to string
         game_count = game_count + 1  # game counter
         if row == "1":
             cur_win_count = cur_win_count + 1
-            cur_win_prob_count = cur_win_count / game_count
+            win_so_far.append(cur_win_count)
+
+            cur_win_prob_count = cur_win_count / game_count / 100
             percetage_win_so_far.append(cur_win_prob_count)
 
-            cur_loss_prob_count = cur_loss_count / game_count
             percetage_loss_so_far.append(cur_loss_prob_count)
-
-            cur_tie_prob_count = cur_tie_count / game_count
-            percetage_tie_so_far.append(cur_tie_prob_count)
+            percetage_tie_so_far.append(cur_tie_prob_count) 
         elif row == "-1":
             cur_loss_count = cur_loss_count + 1
-
-            cur_loss_prob_count = cur_loss_count / game_count
+            loss_so_far.append(cur_loss_count)
+ 
+            cur_loss_prob_count = cur_loss_count / game_count / 100
             percetage_loss_so_far.append(cur_loss_prob_count)
 
-            cur_tie_prob_count = cur_tie_count / game_count
-            percetage_tie_so_far.append(cur_tie_prob_count)
-
-            cur_win_prob_count = cur_win_count / game_count
             percetage_win_so_far.append(cur_win_prob_count)
+            percetage_tie_so_far.append(cur_tie_prob_count) 
         else:  # row == "0"
             cur_tie_count = cur_tie_count + 1
+            tie_so_far.append(cur_tie_count)
 
-            cur_tie_prob_count = cur_tie_count / game_count
+            cur_tie_prob_count = cur_tie_count / game_count / 100
             percetage_tie_so_far.append(cur_tie_prob_count)
 
-            cur_win_prob_count = cur_win_count / game_count
             percetage_win_so_far.append(cur_win_prob_count)
-
-            cur_loss_prob_count = cur_loss_count / game_count
             percetage_loss_so_far.append(cur_loss_prob_count)
-    cnt = range(game_count)
+
     # plots all 3 functions on same graph
     plt.title('SumpremeBot Learning Trend')
-    plt.plot(cnt, percetage_win_so_far, "b", cnt,
-             percetage_loss_so_far, "r", cnt, percetage_tie_so_far, "y")
+    plt.plot(count, percetage_win_so_far, "b", count,
+             percetage_loss_so_far, "r", count, percetage_tie_so_far, "g")
     plt.legend(('Win', 'Loss', 'Tie'),
                loc='upper right')
     plt.ylabel('Win%,Loss%,Tie%')
